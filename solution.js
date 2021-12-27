65da6ad45322b3d20ea0e16fbe5d88ee64acfd16
Krysyx <anthololz31@yahoo.fr>
2021-12-28 00:34:32 +0100

    prettierified

--- solution.js
+++ solution.js
@@ -11,39 +11,48 @@
  */
 
 function queensAttack(n, k, r_q, c_q, obstacles) {
-    // Remove duplicates and store obstacle as a string to make it easier to find any matching obstacles
-    const obs = new Set(obstacles.map(([row, column]) => `${row} ${column}`));
+  // Remove duplicates and store obstacle as a string to make it easier to find any matching obstacles
+  const obs = new Set(obstacles.map(([row, column]) => `${row} ${column}`));
 
-    // Every row/columns coefficients as follows :
-    // left = [0, -1]
-    // right = [0, 1]
-    // top  = [1, 0]
-    // bottom  = [-1, 0]
-    // topLeft  = [1, -1]
-    // topRight = [1, 1]
-    // bottomLeft  = [-1, -1]
-    // bottomRight = [-1, 1]
-    const coefficients = [[0, -1], [0, 1], [1, 0], [-1, 0], [1, -1], [1, 1], [-1, -1]];                            [-1, 1]];
-    
-    return coefficients.reduce((prev, [rCoeff, cCoeff]) => {
-        //Start the row and column count at the specified coefficient
-        let row = r_q + rCoeff;
-        let column = c_q + cCoeff;
-        
-        // make sure that the row and column is always greater than the total number of the board and greater than 0
-        while ((row <= n && row > 0) && (column <= n && column > 0)) {
-            // if our obstacle array has the exact same row/column pair, we are facing an obstacle, thus we return
-            // the current number of exposed cells
-            if (obs.has(`${row} ${column}`)) return prev;
+  // Every row/columns coefficients as follows :
+  // left = [0, -1]
+  // right = [0, 1]
+  // top  = [1, 0]
+  // bottom  = [-1, 0]
+  // topLeft  = [1, -1]
+  // topRight = [1, 1]
+  // bottomLeft  = [-1, -1]
+  // bottomRight = [-1, 1]
+  const coefficients = [
+    [0, -1],
+    [0, 1],
+    [1, 0],
+    [-1, 0],
+    [1, -1],
+    [1, 1],
+    [-1, -1],
+    [-1, 1],
+  ];
 
-            // if there was no obstacle found at this row/col pair, increment exposed cells by 1 
-            // and adjust the row and columns locations
-            ++prev;
-            row += rCoeff;
-            column += cCoeff;
-        }
-        
-        return prev;
+  return coefficients.reduce((prev, [rCoeff, cCoeff]) => {
+    //Start the row and column count at the specified coefficient
+    let row = r_q + rCoeff;
+    let column = c_q + cCoeff;
+
+    // make sure that the row and column is always greater than the total number of the board and greater than 0
+    while (row <= n && row > 0 && column <= n && column > 0) {
+      // if our obstacle array has the exact same row/column pair, we are facing an obstacle, thus we return
+      // the current number of exposed cells
+      if (obs.has(`${row} ${column}`)) return prev;
+
+      // if there was no obstacle found at this row/col pair, increment exposed cells by 1
+      // and adjust the row and columns locations
+      ++prev;
+      row += rCoeff;
+      column += cCoeff;
+    }
+
+    return prev;
     // We start the reducer fn at 0, which will be the starting point of our exposed cells
-    }, 0)
+  }, 0);
 }
